a71283a616a0c21b8078f0bcab751940fb7c0988
gnlgk <161431748+gnlgk@users.noreply.github.com>
2024-07-19 16:47:08 +0900

    Update twosome.py

--- twosome.py
+++ twosome.py
@@ -79,9 +79,14 @@ try:
     print("데이터 저장 중...")
 
 
-    # 데이터를 JSON 파일로 저장
-    with open(filename, 'w', encoding='utf-8') as f:
-        json.dump(ts_data, f, ensure_ascii=False, indent=4)
+   # 데이터를 JSON 파일로 저장
+    if ts_data:
+        print("데이터 저장 중...")
+        with open(filename, 'w', encoding='utf-8') as f:
+            json.dump(ts_data, f, ensure_ascii=False, indent=4)
+        print(f"JSON 파일이 {filename}에 저장되었습니다.")
+    else:
+        print("추출된 데이터가 없습니다.")
 
 except Exception as e:
     print(f"오류 발생: {e}")
