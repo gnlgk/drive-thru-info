68cbad9a6edfd8d485cbbceecc39de55c23a2be1
gnlgk <161431748+gnlgk@users.noreply.github.com>
2024-07-20 22:35:01 +0900

    Update twosome.py

--- twosome.py
+++ twosome.py
@@ -5,6 +5,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import ActionChains
 from bs4 import BeautifulSoup
 from datetime import datetime
 import time
